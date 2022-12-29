97f20ef46ab3c0ceae912a6da536549be26e6ba5
melkamu <zmelkamu918@gmail.com>
2022-12-29 19:13:14 +0300

    it is first project

---
+++ app/src/main/java/com/example/tourguide/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.tourguide;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        int x=4;
+    }
+}
